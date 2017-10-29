44bd2464de0c997f6e28da3a0e82c37916745ab8
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-29 14:30:28 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -20,7 +20,16 @@ module Players
          else
            move = 0
          end
-       else Game::WIN_COMBINATIONS.find do |combo|
+         
+       elsif board.cells[0] && board.cells[8] != " " || board.cells[2] && board.cells[6] != " "
+                 binding.pry
+         if board.position(1) && board.position(9) != token || board.position(3) && board.position(7) != token
+             if board.position(5) != token && !board.taken?(3)
+                 move = 2
+             end
+           end
+
+         else Game::WIN_COMBINATIONS.find do |combo|
            if combo.select{|cell| board.cells[cell] == token}.length==2 && combo.select{|cell| board.position(cell) == " "}.length==1
              move = combo.detect{|cell| board.cells[cell] == " "}
           elsif combo.select{|cell| board.cells[cell] != " " && board.cells[cell] != token}.length == 2 && combo.select{|cell| board.cells[cell] == " "}.length==1
@@ -54,14 +63,7 @@ module Players
           elsif board.cells[7] && board.cells[5] != " " && board.position(8) == board.position(6) && !board.taken?(9)
             move = 8
 
-          elsif board.cells[0] && board.cells[8] != " " || board.cells[2] && board.cells[6] != " "
-            if board.position(1) && board.position(9) != token || board.position(3) && board.position(7) != token
-              if board.position(5) != token && !board.taken?(3)
-                move = 2
-              end
-            end
-          elsif move == nil
-            
+          else
           move = [0,1,2,3,4,5,6,7,8].detect{|cell| board.cells[cell] == " "}
         end
       end
