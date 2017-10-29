797413b229f92c3f1b541fad00452baed6ece1b6
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-29 14:36:33 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -24,6 +24,7 @@ module Players
          binding.pry
          move = 2
        elsif board.cells[2] && board.cells[6] != " " && board.cells[2] && board.cells[6] != token && board.taken?(5) && !board.taken?(2)
+         binding.pry
          move = 2
          else Game::WIN_COMBINATIONS.find do |combo|
            if combo.select{|cell| board.cells[cell] == token}.length==2 && combo.select{|cell| board.position(cell) == " "}.length==1
